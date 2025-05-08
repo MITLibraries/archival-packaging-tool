e878392b5ed810a58a49baa3af7d56fcfdda755f
Graham Hukill <ghukill@gmail.com>
2025-05-08 13:45:27 -0400

    Scaffold HTTP based lambda and SAM testing
    Why these changes are being introduced:
    
    The APT Lambda will primarily be invoked via an HTTP request payload,
    e.g. from an ALB, Function URL, or API Gateway.  As such, it needs
    some handling for parsing the request payload (event) and returning
    a response that will be translated into an HTTP response.
    
    How this addresses that need:
    
    Similar to other lambda projects, a couple of helper methods are
    add generate_result_response() and generate_error_response() that
    are used to return the lambda response.
    
    Additionally, AWS SAM infrastructure has been added to allow
    for testing the lambda locally by sending HTTP requests as if
    the deployed lambda were receiving events from an HTTP invocation.
    
    At this time, only a single action 'ping' is supported by the
    lambda.
    
    Side effects of this change:
    * Lambda can parse HTTP invocations and return valid HTTP
    responses
    
    Relevant ticket(s):
    * https://mitlibraries.atlassian.net/browse/IN-1215

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,50 @@
+import json
+
 import pytest
 
 
 @pytest.fixture(autouse=True)
 def _test_env(monkeypatch):
     monkeypatch.setenv("WORKSPACE", "test")
+    monkeypatch.setenv("SENTRY_DSN", "https://1234567890@00000.ingest.sentry.io/123456")
+    monkeypatch.setenv("CHALLENGE_SECRET", "test-secret")
+
+
+@pytest.fixture
+def valid_event():
+    """Valid event payload for an HTTP invocation."""
+    return {
+        "body": json.dumps(
+            {"action": "ping", "challenge_secret": "test-secret", "verbose": True}
+        ),
+        "requestContext": {"http": {"method": "POST"}},
+    }
+
+
+@pytest.fixture
+def valid_event_direct():
+    """Valid event payload for a direct (non HTTP) invocation."""
+    return {"action": "ping", "challenge_secret": "test-secret", "verbose": True}
+
+
+@pytest.fixture
+def invalid_event():
+    return {
+        "body": json.dumps(
+            {"action": "unknown_action", "challenge_secret": "test-secret"}
+        ),
+        "requestContext": {"http": {"method": "POST"}},
+    }
+
+
+@pytest.fixture
+def invalid_secret_event():
+    return {
+        "body": json.dumps({"action": "ping", "challenge_secret": "wrong-secret"}),
+        "requestContext": {"http": {"method": "POST"}},
+    }
+
+
+@pytest.fixture
+def malformed_event():
+    return {"body": "...", "requestContext": {"http": {"method": "POST"}}}
